a65e6e38dad80e1afaa564191d8fdd8d53470888
Michael Heenan <mick.heenan@razor.co.uk>
2019-10-21 16:13:22 +0100

    Add Dropdown to Admin Index page.

--- MichaelHeenanBlog/Areas/Admin/Pages/DeletePost.cshtml.cs
+++ MichaelHeenanBlog/Areas/Admin/Pages/DeletePost.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MichaelHeenanBlog.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,13 +10,55 @@ namespace MichaelHeenanBlog.Areas.Admin.Pages
 {
     public class DeletePostModel : PageModel
     {
-        public void OnGet()
+        private readonly BlogDbContext _blogDbContext;
+        
+        [BindProperty]
+        public Guid BlogpostId { get; set; }
+
+        public BlogPostEntity BlogPost { get; set; }
+
+        public DeletePostModel(BlogDbContext blogDbContext)
         {
+            _blogDbContext = blogDbContext;
+        }
+        
+        public IActionResult OnGet(Guid blogPostId)
+        {
+            GetBlogPost(blogPostId);
+
+            return Page();
         }
 
-        public void OnPost()
+        public IActionResult OnPost()
         {
+            DeleteBlogPost(BlogpostId);
 
+            return RedirectToPage("/Index", new { area = "admin" });
+        }
+
+        public void GetBlogPost(Guid blogPostId)
+        {
+            BlogPost = _blogDbContext
+               .BlogPosts
+               .Select(b => new BlogPostEntity
+               {
+                   Id = b.Id,
+                   Title = b.Title,
+                   Body = b.Body,
+                   CreatedAt = b.CreatedAt,
+                   Tags = b.Tags,
+                   Comments = b.Comments
+               })
+               .SingleOrDefault(b => b.Id == blogPostId);
+        }
+
+        public void DeleteBlogPost(Guid Id)
+        {
+            var postToDelete =_blogDbContext
+            .BlogPosts
+            .SingleOrDefault(b => b.Id == Id);
+
+            _blogDbContext.Remove(postToDelete);
         }
     }
 }

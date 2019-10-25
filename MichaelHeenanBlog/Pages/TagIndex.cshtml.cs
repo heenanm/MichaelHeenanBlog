628d4bdcc2362921290b59969e8e640fd98c3415
Michael Heenan <mick.heenan@razor.co.uk>
2019-10-25 16:11:10 +0100

    Add TagIndex Page

---
+++ MichaelHeenanBlog/Pages/TagIndex.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MichaelHeenanBlog.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace MichaelHeenanBlog.Pages
+{
+    public class TagIndexModel : PageModel
+    {
+        private readonly BlogDbContext _blogDbContext;
+
+        public TagIndexModel(BlogDbContext blogDbContext)
+        {
+            _blogDbContext = blogDbContext;
+        }
+
+        public void OnGet(Guid tagId)
+        {
+            
+        }
+
+        //private void GetBlogPosts(Guid tagId)
+        //{
+        //    var blogPostsByTag = _blogDbContext
+        //        .BlogPosts
+        //        .Where(b => b.Tags)
+
+
+
+        //}
+    }
+}
